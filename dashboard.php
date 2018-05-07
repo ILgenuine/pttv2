76d8e857f1452c545baa65e878dc98b8e85a4b6b
ilgenuine <infx@live.se>
2018-05-07 12:49:25 +0700

    dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -10,8 +10,10 @@
 
 
               <div class="banner mx-5">
+
                     <div style="background: url(assets/images/bg/header-bg.jpg) fixed;height:400px;width:100%;">
                     </div>
+
                     <div class="banner-title">
                         <div class="banner-text-frame">
                           <font class="banner-title-text">SP PTTGC</font>
@@ -20,24 +22,139 @@
                           <hr class="banner-title-hr">
                         </div>
                     </div>
-
                     <div class="banner-title-2">
                         <div class="banner-text-frame-2">
                           <font class="banner-title-text-2">PARTNERSHIP OF CONSUMABLE CONTRACT</font>
                         </div>
                     </div>
+
+
                     <div class="banner-footer">
-                      <div class="d-flex">
-                        <div class="banner-footer-left">
-                          <i class="fa fa-th-large"></i>
+                      <div class="d-flex py-2">
+                        <div class="mr-auto pl-5">
+                          <i class="fa fa-th-large pr-4"></i>
+                          <font class="text-white">SUMMARY REQUEST STATUS</font>
                         </div>
-                        <div class="banner-footer-right">
-                          d
+                        <div class="ml-auto pr-5">
+                          <a href="#"><span class="flaticon-letter text-white"></span></a>
+                          <a href="#"><span class="flaticon-download-button text-white"></span></a>
+                          <a href="#"><span class="flaticon-printing-tool text-white"></span></a>
+                        </div>
+                      </div>
+                    </div>
+
+
+                </div>
+
+                <div class="banner mx-5">
+                    <div class="banner-summary my-1">
+                      <div class="d-flex py-2">
+                        <div class="summary-col">
+                          <div class="summary-frame text-center">
+                            <font class="text-white">NEW REQUEST</font>
+                            <div class="summary-number green">18</div>
+                          </div>
+                        </div>
+                        <div class="summary-col summary-border">
+                          <div class="summary-frame text-center">
+                            <font class="text-white">CANCEL</font>
+                            <div class="summary-number red">2</div>
+                          </div>
+                        </div>
+                        <div class="summary-col summary-border">
+                          <div class="summary-frame text-center">
+                            <font class="text-white">ORDER ON PROCESS</font>
+                            <div class="summary-number orange">9</div>
+                          </div>
+                        </div>
+                        <div class="summary-col summary-border">
+                          <div class="summary-frame text-center">
+                            <font class="text-white">ACCEPT & WAITING FOR PR</font>
+                            <div class="summary-number grey">1</div>
+                          </div>
+                        </div>
+                        <div class="summary-col summary-border">
+                          <div class="summary-frame text-center">
+                            <font class="text-white">COMPLETE JOB</font>
+                            <div class="summary-number blue">1</div>
+                          </div>
                         </div>
                       </div>
                     </div>
                 </div>
 
+
+              <div class="container-fluid dashboard-container">
+                <div class="row">
+                  <div class="col-8">
+                    <div class="row">
+                      <div class="col-4 px-3 py-3">
+                          <div class="menu-frame text-center">
+                            <a href="#" class="text-black icon-hover">
+                              <img class="dashboard-icon" src="assets/images/dashboardicon/bell.png" alt="">
+                              <div class="mt-5">
+                                <font>CANCEL REQUEST</font>
+                              </div>
+                            </a>
+                          </div>
+                      </div>
+                      <div class="col-4 px-3 py-3">
+                          <div class="menu-frame text-center">
+                            <a href="#" class="text-black icon-hover">
+                              <img class="dashboard-icon" src="assets/images/dashboardicon/growth.png" alt="">
+                              <div class="mt-5">
+                                <font>MONTHY REQUEST</font>
+                              </div>
+                            </a>
+                          </div>
+                      </div>
+                      <div class="col-4 px-3 py-3">
+                          <div class="menu-frame text-center">
+                            <a href="#" class="text-black icon-hover">
+                              <img class="dashboard-icon" src="assets/images/dashboardicon/report.png" alt="">
+                              <div class="mt-5">
+                                <font>SAMPLE ICON</font>
+                              </div>
+                            </a>
+                          </div>
+                      </div>
+                      <div class="col-4 px-3 py-3">
+                          <div class="menu-frame text-center">
+                            <a href="#" class="text-black icon-hover">
+                              <img class="dashboard-icon" src="assets/images/dashboardicon/presentation.png" alt="">
+                              <div class="mt-5">
+                                 <font>SAMPLE ICON</font>
+                              </div>
+                            </a>
+                          </div>
+                      </div>
+                      <div class="col-4 px-3 py-3">
+                          <div class="menu-frame text-center">
+                            <a href="#" class="text-black icon-hover">
+                              <img class="dashboard-icon" src="assets/images/dashboardicon/coins.png" alt="">
+                              <div class="mt-5">
+                                <font>SAMPLE ICON</font>
+                              </div>
+                            </a>
+                          </div>
+                      </div>
+                      <div class="col-4 px-3 py-3">
+                          <div class="menu-frame text-center">
+                            <a href="#" class="text-black icon-hover">
+                              <img class="dashboard-icon" src="assets/images/dashboardicon/diploma.png" alt="">
+                              <div class="mt-5">
+                                <font>SAMPLE ICON</font>
+                              </div>
+                            </a>
+                          </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-4">
+                    <div class="calendar"> calendar</div>
+                  </div>
+                </div>
+              </div>
 
     <?php include_once('inc/footer.php'); ?>
 
